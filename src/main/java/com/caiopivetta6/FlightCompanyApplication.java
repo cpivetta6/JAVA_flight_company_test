a5b597295ddd5c4fc6a7af5768525e119f55220b
Caio Pivetta <caiopivetta6@gmail.com>
2023-01-14 16:16:16 +0100

    Project Created

---
+++ src/main/java/com/caiopivetta6/FlightCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.caiopivetta6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiopivetta6/FlightCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiopivetta6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
