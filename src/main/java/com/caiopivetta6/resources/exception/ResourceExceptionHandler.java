950c05f2810766a705dc06facdad796f5490d6d4
Caio Pivetta <caiopivetta6@gmail.com>
2023-01-16 18:02:19 +0100

    Resource and service Exceptions

---
+++ src/main/java/com/caiopivetta6/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.caiopivetta6.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.caiopivetta6.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+	
+	
+}
