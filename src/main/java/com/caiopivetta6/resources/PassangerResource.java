06b8313fe467e0f352b06cd70d0632086c652447
Caio Pivetta <caiopivetta6@gmail.com>
2023-01-29 11:28:43 +0100

    Service Exception database added

--- src/main/java/com/caiopivetta6/resources/PassangerResource.java
+++ src/main/java/com/caiopivetta6/resources/PassangerResource.java
@@ -1,11 +1,18 @@
 package com.caiopivetta6.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.caiopivetta6.domain.Passanger;
 import com.caiopivetta6.services.PassangerService;
@@ -22,6 +29,27 @@ public class PassangerResource {
 		
 		Passanger obj = service.find(id);
 		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Passanger> insert (@RequestBody Passanger obj){
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Passanger> update (@PathVariable Integer id, @RequestBody Passanger obj){
+		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 	
--- src/main/java/com/caiopivetta6/services/PassangerService.java
+++ src/main/java/com/caiopivetta6/services/PassangerService.java
@@ -3,12 +3,17 @@ package com.caiopivetta6.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.caiopivetta6.domain.Passanger;
-import com.caiopivetta6.domain.Reserve;
 import com.caiopivetta6.repository.PassangerRepository;
+import com.caiopivetta6.services.exception.DatabaseException;
 import com.caiopivetta6.services.exception.ObjectNotFoundException;
+import com.caiopivetta6.services.exception.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PassangerService {
@@ -25,4 +30,39 @@ public class PassangerService {
 				"Object not found! Id: " + id + ", Tipo: " + Passanger.class.getName()));
 
 	}
+	
+	public Passanger insert(Passanger obj) {
+		repository.save(obj);
+		return obj;
+	}
+	
+	
+	public void delete(Integer id) {
+		try {
+		repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Passanger update(Integer id, Passanger obj) {
+		try {
+		Passanger entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
+	}
+	
+	private void updateData(Passanger entity, Passanger obj) {
+		entity.setName(obj.getName());
+		entity.setGenre(obj.getGenre());
+		entity.setPhone(obj.getPhone());
+	}
+	
+	
 }
